d40f0c8313cc5c91b3853b3daf7c9162234dae9c
배정현 <jhbae@the-51.com>
2023-09-04 13:29:18 +0900

    Type_B scroll

--- type_B/assets/js/scriptSub.js
+++ type_B/assets/js/scriptSub.js
@@ -145,7 +145,7 @@ gsap.to($scroll2, {
     trigger: ".scroll2",
     start: "top 100px",
     endTrigger: ".scroll2",
-    end: "bottom 160px",
+    end: "bottom 196px",
     pin: true, // left 클래스 고정
     scrub: true, // 스크롤에 따라 부드럽게 이동
   },
@@ -157,7 +157,7 @@ gsap.to($scroll3, {
     trigger: ".scroll3",
     start: "top 100px",
     endTrigger: ".scroll3",
-    end: "bottom 160px",
+    end: "bottom 196px",
     pin: true, // left 클래스 고정
     scrub: true, // 스크롤에 따라 부드럽게 이동
   },
@@ -246,4 +246,53 @@ function careersHoverEvent() {
 }
 $(document).ready(function () {
   careersHoverEvent();
+});
+
+class Scrooth {
+  constructor({element = window, strength=10, acceleration = 1.2,deceleration = 0.975}={}) {
+    this.element = element;
+    this.distance = strength;
+    this.acceleration = acceleration;
+    this.deceleration = deceleration;
+    this.running = false;
+
+    this.element.addEventListener('wheel', this.scrollHandler.bind(this), {passive: false});
+    this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {passive: false});
+    this.scroll = this.scroll.bind(this);
+  }
+
+  scrollHandler(e) {
+    e.preventDefault();
+
+    if (!this.running) {
+      this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+      this.running = true;
+      this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+      this.isDistanceAsc = true;
+      this.scroll();
+    } else {
+      this.isDistanceAsc = false;
+      this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+    }
+  }
+
+  scroll() {
+    if (this.running) {
+      this.currentDistance *= this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+      Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false ? this.running = false : 1;
+      Math.abs(this.currentDistance) >= Math.abs(this.distance) ? this.isDistanceAsc = false : 1;
+
+      this.top += this.currentDistance;
+      this.element.scrollTo(0, this.top);
+      
+      requestAnimationFrame(this.scroll);
+    }
+  }
+}
+
+const scroll = new Scrooth({
+  element: window,
+  strength: 20,
+  acceleration: 1.5,
+  deceleration: 0.975,
 });
